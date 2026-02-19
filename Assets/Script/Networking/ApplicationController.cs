c80bafb2d2c635896f5a419a44da0a27209616ae
SaChan24 <taripoom_2224@hotmail.com>
2026-02-19 15:15:57 +0700

    Week6

---
+++ Assets/Script/Networking/ApplicationController.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ApplicationController : MonoBehaviour
+{
+    [SerializeField] private ClientSingleton clientPrefab;
+
+    async void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        await LaunchInMode(SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null);
+    }
+
+    private async Task LaunchInMode(bool isDedicatedServer)
+    {
+        if (isDedicatedServer)
+        {
+            
+        }
+        else
+        {
+            ClientSingleton clientSingleton = Instantiate(clientPrefab);
+
+            await clientSingleton.CreateClient();
+        }
+    }
+    
+}
